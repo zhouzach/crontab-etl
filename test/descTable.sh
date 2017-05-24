93932513acdc18b92a3cdfb019f851560337bed6
Jeffrey Fan <ennea8@gmail.com>
2017-05-24 14:27:57 +0800

    optimize scripts

--- test/descTable.sh
+++
@@ -1,4 +0,0 @@
-
-host=${1}
-table=${2}
-impala-shell -i ${host} -q "desc formatted ${table};"
--- test/select-bp-pt.sh
+++ test/select-bp-pt.sh
@@ -1,2 +1,2 @@
 
-impala-shell -i 10.9.0.11:32400 -q "select * from ih_aijiakang.ih_blood_pressure_pt;"
+impala-shell -i 10.9.0.11:32400 -q "select * from ih_aijiakang.ih_blood_pressure_ptt;"
